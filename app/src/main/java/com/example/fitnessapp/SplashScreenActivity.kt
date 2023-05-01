4d7f26e76e7e63522a89ed26bfb2e961aa4776db
viren <virenvariya7@gmail.com>
2023-05-01 23:16:07 +0530

    Update : Splash Screen color change effect

--- app/src/main/java/com/example/fitnessapp/SplashScreenActivity.kt
+++ app/src/main/java/com/example/fitnessapp/SplashScreenActivity.kt
@@ -4,18 +4,53 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private val colorList = arrayOf(
+        R.color.purple1, R.color.green, R.color.red,
+        R.color.purple2, R.color.green
+    )
+
+    private var colorIndex = 0
+
+    private val colorHandler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        // Add a delay before moving to the next activity
+        startColorChange()
+
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 3000) // 3 seconds delay
+        }, 3000)
+    }
+
+    private fun startColorChange() {
+        colorHandler.post(object : Runnable {
+            override fun run() {
+                val color = ContextCompat.getColor(this@SplashScreenActivity, colorList[colorIndex])
+                findViewById<View>(android.R.id.content).setBackgroundColor(color)
+
+                colorIndex = (colorIndex + 1) % colorList.size
+
+                colorHandler.postDelayed(this, 1000)
+            }
+        })
+    }
+
+    private fun stopColorChange() {
+        colorHandler.removeCallbacksAndMessages(null)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopColorChange()
     }
 }
